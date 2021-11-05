8304cafd73f19dc7acc1475da3cb3454356dc051
BigMasonFang <524737106@qq.com>
2021-11-05 18:56:37 +0800

    [feature] continue on structure

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,7 +7,7 @@ from loguru import logger
 from starlette.config import Config
 from starlette.datastructures import CommaSeparatedStrings, Secret
 
-from app.config.logging import InterceptHandler
+from app.core.logging import InterceptHandler
 
 API_PREFIX = "/api"
 
